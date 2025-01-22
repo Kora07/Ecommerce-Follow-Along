2d8fe4598a557ce99dfacf79c4619d5c35871d78
Pramod Magesh <pramodmagesh20@gmail.com>
2025-01-22 14:55:57 +0530

    Login page created

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import { Route, Routes } from 'react-router-dom'
+import Login from './Component/Login'
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+		<Route path='/login' element={<Login/>}/>
+	  </Routes>
+    </>
+  )
+}
+
+export default App
