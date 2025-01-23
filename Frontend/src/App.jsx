f9ad11077eab3cf55ab4ac4f9eb699d63a7443cb
Pramod Magesh <pramodmagesh20@gmail.com>
2025-01-23 10:17:29 +0530

    Idek what change I made here

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,13 +6,13 @@ import Login from './Component/Login'
 function App() {
 
 
-  return (
-    <>
-      <Routes>
-		<Route path='/login' element={<Login/>}/>
-	  </Routes>
-    </>
-  )
+	return (
+		<>
+			<Routes>
+				<Route path='/login' element={<Login/>}/>
+			</Routes>
+		</>
+	)
 }
 
 export default App
