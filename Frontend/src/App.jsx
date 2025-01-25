7583902c9aa4781d2d1175b67b6623fa536b8c83
Pramod Magesh <pramodmagesh20@gmail.com>
2025-01-25 13:44:23 +0530

    Avatar and sign up page created?

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
-
+import "./app.css"
 import { Route, Routes } from 'react-router-dom'
 import Login from './Component/Login'
+import SignUp from './Component/signUp'
+import Home from './Component/home'
+
 
 function App() {
 
@@ -9,10 +12,12 @@ function App() {
 	return (
 		<>
 			<Routes>
+				<Route path='/' element={<Home/>}/>
 				<Route path='/login' element={<Login/>}/>
+				<Route path='/sign-up' element={<SignUp/>}/>
 			</Routes>
 		</>
 	)
 }
 
-export default App
+export default App;
--- Frontend/src/Component/Login.jsx
+++ Frontend/src/Component/Login.jsx
@@ -4,7 +4,7 @@ import '../index.css';
 
 function Login() {
 
-    const [passwordVisibility, setPasswordVisibiity] = useState(false);
+    const [passwordVisibility, setPasswordVisibiity] = useState(true);
     const toggleVisibility = () => {
         setPasswordVisibiity(!passwordVisibility);
         console.log(passwordVisibility)
@@ -12,6 +12,7 @@ function Login() {
 
     return (
         <>
+        <form>
           <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
@@ -77,12 +78,13 @@ function Login() {
     
               <p className="mt-10 text-center text-sm/6 text-gray-500">
                 Not a member?{' '}
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                <a href="/sign-up" className="font-semibold text-indigo-600 hover:text-indigo-500">
                   Sign Up
                 </a>
               </p>
             </div>
           </div>
+          </form>
         </>
     )
 }
