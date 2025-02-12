7fbb5ee495ee96abd361ec92871d023f3bbfe976
Pramod Magesh <pramodmagesh20@gmail.com>
2025-02-12 12:34:45 +0530

    Minor changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,8 @@ import "./app.css"
 import { Route, Routes } from 'react-router-dom'
 import Login from './Component/Login'
 import SignUp from './Component/signUp'
-import Home from './Component/home'
-import {ProductForm} from './Component/productForm'
+import Homepage from './Component/home'
+import ProductForm from './Component/productForm'
 
 
 
@@ -15,7 +15,7 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path='/' element={<Home/>}/>
+				<Route path='/' element={<Homepage/>}/>
 				<Route path='/login' element={<Login/>}/>
 				<Route path='/sign-up' element={<SignUp/>}/>
 				<Route path='/productForm' element={<ProductForm/>}/>
