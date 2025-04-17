d8a4fe76163463289bffd50fc852724f835e5cc5
Pramod Magesh <pramodmagesh20@gmail.com>
2025-04-17 12:41:49 +0530

    Updated index

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ require('dotenv').config({
     path:'./src/Config/.env'
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const URL = process.env.URL;
 
 app.get('/', (req, res) => {    
